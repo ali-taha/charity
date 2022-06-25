50282eecd8807a9fc2ef0663384b06e5cfaa8800
alidtaha@gmail.com <alidtaha@gmail.com>
2022-06-25 22:21:05 +0430

    refactor code

--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,25 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.forms import ChoiceField
 
+from .validators import phone_validator
 
-GENDER_CHOICES = [
-    ('M','Man'),
-    ('F','Female'),
-]
 
 class User(AbstractUser):
-    address = models.TextField(blank=True, null=True)
+    class Gender(models.TextChoices):
+        MALE = 'M', 'Male'
+        FEMALE = 'F', 'Female'
+        UNSET = 'MF', 'Unset'
+
+    phone = models.CharField(max_length=15, validators=[phone_validator], blank=True)
+    address = models.TextField(blank=True)
+    gender = models.CharField(max_length=2, choices=Gender.choices, default=Gender.UNSET)
     age = models.PositiveSmallIntegerField(blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-    gender = models.CharField(max_length=1,choices=GENDER_CHOICES, blank=True, null=True)
-    phone = models.CharField(max_length=15, blank=True, null=True)
+    description = models.TextField(blank=True)
+
+    @property
+    def is_benefactor(self):
+        return hasattr(self, 'benefactor')
+
+    @property
+    def is_charity(self):
+        return hasattr(self, 'charity')
