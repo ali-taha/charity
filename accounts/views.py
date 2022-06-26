1268749a1511b5d5a70ef6262e75d0999b66e384
alidtaha@gmail.com <alidtaha@gmail.com>
2022-06-26 18:40:48 +0430

    user register

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView
+
 
 from .serializers import UserSerializer
 
@@ -18,5 +20,7 @@ class LogoutAPIView(APIView):
         )
 
 
-class UserRegistration(APIView):
-    pass
+class UserRegistration(CreateAPIView):
+    serializer_class = UserSerializer
+
+    
